3c7389e07fc66f41dcbd5998bd8bf5554a9832a0
ivanisovich <57730983+ivanisovich@users.noreply.github.com>
2024-03-12 23:35:27 +0500

    added tetris mini game

--- scripts/draw-section.js
+++ scripts/draw-section.js
@@ -1,11 +1,11 @@
 const paintCanvas = document.querySelector('.js-paint');
-const context = paintCanvas.getContext('2d');
-context.lineCap = 'round';
+const drawContext = paintCanvas.getContext('2d');
+drawContext.lineCap = 'round';
 
 const colorPicker = document.querySelector('.js-color-picker');
 
 colorPicker.addEventListener('change', event => {
-    context.strokeStyle = event.target.value;
+    drawContext.strokeStyle = event.target.value;
 });
 
 const lineWidthRange = document.querySelector('.js-line-range');
@@ -14,7 +14,7 @@ const lineWidthLabel = document.querySelector('.js-range-value');
 lineWidthRange.addEventListener('input', event => {
     const width = event.target.value;
     lineWidthLabel.innerHTML = width;
-    context.lineWidth = width;
+    drawContext.lineWidth = width;
 });
 
 let x = 0, y = 0;
@@ -30,10 +30,10 @@ const drawLine = event => {
     if (isMouseDown) {
         const newX = event.offsetX;
         const newY = event.offsetY;
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(newX, newY);
-        context.stroke();
+        drawContext.beginPath();
+        drawContext.moveTo(x, y);
+        drawContext.lineTo(newX, newY);
+        drawContext.stroke();
         [x, y] = [newX, newY];
     }
 };
@@ -49,10 +49,10 @@ const drawLineTouch = event => {
         const rect = paintCanvas.getBoundingClientRect();
         const newX = event.touches[0].clientX - rect.left;
         const newY = event.touches[0].clientY - rect.top;
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(newX, newY);
-        context.stroke();
+        drawContext.beginPath();
+        drawContext.moveTo(x, y);
+        drawContext.lineTo(newX, newY);
+        drawContext.stroke();
         [x, y] = [newX, newY];
     }
 };
@@ -66,9 +66,9 @@ const resizeCanvas = () => {
     paintCanvas.width = containerWidth;
     paintCanvas.height = containerHeight;
 
-    context.lineCap = 'round';
-    context.strokeStyle = colorPicker.value;
-    context.lineWidth = lineWidthRange.value;
+    drawContext.lineCap = 'round';
+    drawContext.strokeStyle = colorPicker.value;
+    drawContext.lineWidth = lineWidthRange.value;
 };
 const stopDrawingTouch = () => { isMouseDown = false; };
 
